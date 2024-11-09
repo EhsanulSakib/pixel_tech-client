ca8725ee9dd7a08c8064468fca8ac4f11c0996d1
Kazi Ehsanul Haque <ehsanulsakib.professional@gmail.com>
2024-11-09 22:38:27 +0600

    edgestore setup

---
+++ src/app/api/edgestore/[...edgestore]/route.ts
@@ -0,0 +1,96 @@
+import { initEdgeStore } from '@edgestore/server';
+import { createEdgeStoreNextHandler } from '@edgestore/server/adapters/next/app';
+
+const es = initEdgeStore.create();
+
+/**
+ * This is the main router for the Edge Store buckets.
+ */
+const edgeStoreRouter = es.router({
+  publicFiles: es
+    .fileBucket()
+    /**
+     * return `true` to allow upload
+     * By default every upload from your app is allowed.
+     */
+    .beforeUpload(({ ctx, input, fileInfo }) => {
+      console.log('beforeUpload (publicFiles)', ctx, input, fileInfo);
+      return true; // allow upload
+    })
+    /**
+     * return `true` to allow delete
+     * This function must be defined if you want to delete files directly from the client.
+     */
+    .beforeDelete(({ ctx, fileInfo }) => {
+      console.log('beforeDelete (publicFiles)', ctx, fileInfo);
+      return true; // allow delete
+    }),
+
+  profileAndCoverImages: es
+    .fileBucket()
+    /**
+     * return `true` to allow upload
+     * By default every upload from your app is allowed.
+     */
+    .beforeUpload(({ ctx, input, fileInfo }) => {
+      console.log('beforeUpload (profileAndCoverImages)', ctx, input, fileInfo);
+      return true; // allow Resume upload
+    })
+    /**
+     * return `true` to allow delete
+     * This function must be defined if you want to delete files directly from the client.
+     */
+    .beforeDelete(({ ctx, fileInfo }) => {
+      console.log('beforeDelete (profileAndCoverImages)', ctx, fileInfo);
+      return true; // allow Resume delete
+    }),
+
+  userResumes: es
+    .fileBucket()
+    /**
+     * return `true` to allow upload
+     * By default every upload from your app is allowed.
+     */
+    .beforeUpload(({ ctx, input, fileInfo }) => {
+      console.log('beforeUpload (userResumes)', ctx, input, fileInfo);
+      return true; // allow Resume upload
+    })
+    /**
+     * return `true` to allow delete
+     * This function must be defined if you want to delete files directly from the client.
+     */
+    .beforeDelete(({ ctx, fileInfo }) => {
+      console.log('beforeDelete (userResumes)', ctx, fileInfo);
+      return true; // allow Resume delete
+    }),
+
+  employerCompanyLogo: es
+    .fileBucket()
+    /**
+     * return `true` to allow upload
+     * By default every upload from your app is allowed.
+     */
+    .beforeUpload(({ ctx, input, fileInfo }) => {
+      console.log('beforeUpload (employerCompanyLogo)', ctx, input, fileInfo);
+      return true; // allow Resume upload
+    })
+    /**
+     * return `true` to allow delete
+     * This function must be defined if you want to delete files directly from the client.
+     */
+    .beforeDelete(({ ctx, fileInfo }) => {
+      console.log('beforeDelete (employerCompanyLogo)', ctx, fileInfo);
+      return true; // allow Resume delete
+    }),
+});
+
+const handler = createEdgeStoreNextHandler({
+  router: edgeStoreRouter,
+});
+
+export { handler as GET, handler as POST, handler as DELETE };
+
+/**
+ * This type is used to create the type-safe client for the frontend.
+ */
+export type EdgeStoreRouter = typeof edgeStoreRouter;
